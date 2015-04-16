556190b4c99308ffecf3d9db3526640566327b46
nelson4722 <nelson4722@gmail.com>
2015-04-16 16:38:13 -0300

    traslado tarea 2

---
+++ Tarea02/Accesos.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+#include<fstream>
+#include "libreria/split.h"
+using namespace std;
+
+int main(/*int argc, char *argv[]*/){
+char caracteres[20000];
+string fechain[20];
+string fechaout[20];
+ifstream fe("access.log");
+
+while(!fe.eof()){
+fe.getline(caracteres, 20000); //lee la linea completa
+if(strstr(caracteres,"16/Mar/2015"))
+ 		{
+    vector<string> TempBuff(0);
+    int TotalVector;
+    split(caracteres, *" ",  TempBuff, TotalVector);
+    cout << TempBuff[0] << endl;
+    //cout << TempBuff[3] << endl;
+    //cout << TempBuff[4] << endl;
+    //cout << TempBuff[5] <<" "<<TempBuff[6] <<" "<<TempBuff[7] << endl;
+    cout << TempBuff[8] << endl;
+   // cout <<TempBuff[9] << endl;
+    //cout << TempBuff[10] << endl;
+    //cout << TempBuff[11] << endl;
+
+    split(TempBuff[3], *"[",  TempBuff, TotalVector);
+    //cout << TempBuff[1] << endl;
+    split(TempBuff[1], *":",  TempBuff, TotalVector);
+    cout << TempBuff[0] << endl;
+    cout << "-----" << endl;
+ 		}
+
+//cout<< "----------" << endl;
+}
+/*
+while (!feof(fp))
+ 	{
+ 		fscanf(fp,"%s",caracteres);
+
+
+
+
+ 	}
+ 	*/
+ 	fe.close();
+        system("PAUSE");
+        //fclose(fp);
+  }
---
+++ Tarea02/libreria/split.h
@@ -0,0 +1,43 @@
+#ifndef SPLIT_H_INCLUDED
+#define SPLIT_H_INCLUDED
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<fstream>
+#include <vector>
+using namespace std;
+
+int Cuenta(string s, const char Separadorr, int &TotalChars) {
+    for (int i = 0; i < s.size(); i++)
+        if (s[i] == Separadorr) TotalChars++;
+}
+void split(string Linea, char Separador, vector<string> &TempBuff, int &TotalVector) {
+    TempBuff.resize(0);
+    TotalVector = 0;
+    int Nums = -1;
+    int NumPos = -1;
+    int ValorNum = 0;
+    int TotalChars = 0;
+    int TotalEspacios = Linea.length();
+    string Valor;
+    Cuenta(Linea, Separador, TotalChars);
+    if (TotalChars != 0) {
+        while (Nums < TotalChars) {
+            Nums++;
+            ValorNum = Linea.find(Separador, NumPos + 1);
+            Valor = Linea.substr(NumPos + 1,ValorNum);
+            Valor = Valor.substr(0, Valor.find_first_of(Separador));
+            TempBuff.push_back(Valor);
+            NumPos = ValorNum;
+            TotalEspacios++;
+        }
+        TotalVector = TempBuff.size();
+    }
+    else {
+        //TempBuff.push_back(Linea.substr(0, Linea.find_first_of(Separador)));
+        TotalVector = 0;
+    }
+}
+
+#endif // SPLIT_H_INCLUDED
