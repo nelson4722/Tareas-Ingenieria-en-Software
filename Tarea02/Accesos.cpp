c48054edf5118b16b43e9d41e624b20bc08b2104
nelson4722 <nelson4722@gmail.com>
2015-04-18 16:57:54 -0300

    validaciones y rangos de fechas

--- Tarea02/Accesos.cpp
+++ Tarea02/Accesos.cpp
@@ -8,117 +8,140 @@
 #include "libreria/cambioFecha.h"
 using namespace std;
 
-void cargarArchivo(char *caracteres,char *inicio,ifstream &fe, vector<string> cant_estados,vector<string> estados )
+void Implementacion(char *caracteres,char *inicio,char *fin,ifstream &fe, vector<string> cant_estados,vector<string> estados )
 {
+  bool marca=false;
   while(!fe.eof()){
 fe.getline(caracteres, 20000); //lee la linea completa
-if(strstr(caracteres,inicio))
+
+if(strstr(caracteres,inicio)) marca=true;
+if(strstr(caracteres,fin)) marca=false;
+
+if(marca)
  		{
-    vector<string> TempBuff(0);
-    int TotalVector;
-    split(caracteres, *" ",  TempBuff, TotalVector);
-    cout << TempBuff[0] << endl;
-    //cout << TempBuff[3] << endl;
-    //cout << TempBuff[4] << endl;
-    //cout << TempBuff[5] <<" "<<TempBuff[6] <<" "<<TempBuff[7] << endl;
-    cout << TempBuff[8] << endl;
+            vector<string> TempBuff(0);
+            int TotalVector;
+            split(caracteres, *" ",  TempBuff, TotalVector);
 
-   // cout <<TempBuff[9] << endl;
-    //cout << TempBuff[10] << endl;
-    //cout << TempBuff[11] << endl;
-    cant_estados.push_back(TempBuff[8]); //mete todos los estados en el vector cant_estados
+        if(!TempBuff.empty())
+            {
+                cout << TempBuff[0] << endl;
+                //cout << TempBuff[3] << endl;
+                //cout << TempBuff[4] << endl;
+                //cout << TempBuff[5] <<" "<<TempBuff[6] <<" "<<TempBuff[7] << endl;
+                cout << TempBuff[8] << endl;
 
-            if(estados.empty()){
-                estados.push_back(TempBuff[8]);
-            }
+                // cout <<TempBuff[9] << endl;
+                //cout << TempBuff[10] << endl;
+                //cout << TempBuff[11] << endl;
+                cant_estados.push_back(TempBuff[8]); //mete todos los estados en el vector cant_estados
 
-            for(int i=0;i<estados.size();i++){ //busca el estado en el vector estado
+            if(estados.empty())
+                {
+                    estados.push_back(TempBuff[8]);
+                }
+
+            for(int i=0;i<estados.size();i++)//busca el estado en el vector estado
+            {
 
                 if(estados[i]==TempBuff[8]){
                     i=estados.size();
-                }
-                else{
-                    if(i==estados.size()-1){
+                 }
+                else
+                {
+                    if(i==estados.size()-1)
+                        {
                         estados.push_back(TempBuff[8]);
-                    }
+                        }
                 }
             }
 
-    split(TempBuff[3], *"[",  TempBuff, TotalVector);
-    //cout << TempBuff[1] << endl;
-    split(TempBuff[1], *":",  TempBuff, TotalVector);
-    cout << TempBuff[0] << endl;
-    cout << "-----" << endl;
- 		}
-}
-}
+            split(TempBuff[3], *"[",  TempBuff, TotalVector);
+            //cout << TempBuff[1] << endl;
+            split(TempBuff[1], *":",  TempBuff, TotalVector);
+            cout << TempBuff[0] << endl;
+            cout << "-----" << endl;
+            }
+        }
+    }
 
-int main(int argc, char *argv[])
-{
-char caracteres[20000];
-char fechain[20];
-char fechaout[20];
-int entrada=0,i=0;
-vector<string> estados; //vector con los estados sin repeticion
-vector<string> cant_estados; //vector con todos los estados repetidos incluidos
-ifstream fe("access.log");
-/*int argc=20;
-char *argv[20];
-argv[1]= "-so";
-   argv[4]="access.log";
-   argv[2]="2015-03-16";
-   argv[3]="2015-03-16";*/
-   cout<<"Abriendo archivo"<<argv[0]<<".exe"<<endl;
+    int ct=0;
 
-for(i=0;i<argc;i++)
-{
-   if(strcmp(argv[1],"-so")==0 && strcmp(argv[4],"access.log")==0) entrada = 1;
-   else if(strcmp(argv[1],"-geo")==0 && strcmp(argv[4],"access.log")==0) entrada = 2;
-   else if(strcmp(argv[1],"-v")==0) entrada = 3;
-}
-if(entrada == 0)
-{
-cout<<"\nOpcion no valida, ejecute nuevamente el programa\n";
-// getch();
-return 0;
-}else if(entrada == 1) //OPCION -g
-{
-    string a=cambioFecha(argv[2]);
-    string b=cambioFecha(argv[3]);
-     char *inicio=strdup(a.c_str());
-     cargarArchivo(caracteres,inicio,fe,cant_estados,estados);
-     int ct=0;
-
-        for(int i=0;i<estados.size();i++){ //imprime los estados
-
-            for(int k=0;k<cant_estados.size();k++){
-
-                if(estados[i]==cant_estados[k]){
+        for(int i=0;i<estados.size();i++)//imprime los estados
+         {
+            for(int k=0;k<cant_estados.size();k++)
+               {
+                if(estados[i]==cant_estados[k])
+                  {
                     ct++;
-                }
-            }
+                  }
+              }
             cout << estados[i] << " : " << ct << endl;
             ct = 0;
         }
 
         cout <<  "total de estados: " << cant_estados.size() << endl; //borrar despues
-     //char *fin=strdup(b.c_str());
-    //cout<<"OPCION -g: SORTEO"<<endl;
 }
-else if(entrada == 3) //OPCION -v
-{
-    cout<<"OPCION -v: INTEGRANTES"<<endl;
-    cout<<"Nelson Alegría"<<endl;
-    cout<<"David Olivero"<<endl;
-    cout<<"Nicolas Vera"<<endl;
-    printf("Fecha de compilacion: %s - %s\n\n ", __DATE__, __TIME__);//Fecha de compilacion.
-    EXIT_SUCCESS;
-}
-else
-{
-printf("\nOpcion no valida, ejecute nuevamente el programa\n");
-//getch();
-return EXIT_SUCCESS;
 
-}
+int main(int argc, char *argv[])
+{
+    char caracteres[20000];
+    int entrada=0,i=0;
+    vector<string> estados; //vector con los estados sin repeticion
+    vector<string> cant_estados; //vector con todos los estados repetidos incluidos
+    ifstream fe("access.log");
+
+    /*int argc=20;
+    char *argv[20];
+    argv[1]= "-so";
+   argv[4]="access.log";
+   argv[2]="2015-03-16";
+   argv[3]="2015-03-16";*/
+   cout<<"Abriendo archivo"<<argv[0]<<".exe"<<endl;
+        char *inicio=strdup(cambioFecha(argv[2]).c_str());
+        char *f= strdup(agregarDia(argv[3]).c_str());
+        char *fin=strdup(cambioFecha(f).c_str());
+    for(i=0;i<argc;i++)
+    {
+        if(strcmp(argv[1],"-so")==0 && strcmp(argv[4],"access.log")==0) entrada = 1;
+        else if(strcmp(argv[1],"-geo")==0 && strcmp(argv[4],"access.log")==0) entrada = 2;
+        else if(strcmp(argv[1],"-v")==0) entrada = 3;
+
+    }
+
+    if(entrada == 0)
+    {
+        cout<<"\nOpcion no valida, ejecute nuevamente el programa\n";
+        // getch();
+        return 0;
+    }
+    else if(entrada == 1) //OPCION -g
+    {
+        if(compara(argv[2],argv[3])>0) cout<<"la fecha de termino es mayor a la de inicio"<<endl;
+        else Implementacion(caracteres,inicio,fin,fe,cant_estados,estados);
+
+        //cout<<"OPCION -g: SORTEO"<<endl;
+    }
+    else if(entrada == 2) //OPCION -g
+    {
+        if(compara(argv[2],argv[3])>0) cout<<"la fecha de termino es mayor a la de inicio"<<endl;
+        else Implementacion(caracteres,inicio,fin,fe,cant_estados,estados);
+        //cout<<"OPCION -g: SORTEO"<<endl;
+    }
+    else if(entrada == 3) //OPCION -v
+    {
+        cout<<"OPCION -v: INTEGRANTES"<<endl;
+        cout<<"Nelson Alegría"<<endl;
+        cout<<"David Olivero"<<endl;
+        cout<<"Nicolas Vera"<<endl;
+        printf("Fecha de compilacion: %s - %s\n\n ", __DATE__, __TIME__);//Fecha de compilacion.
+        EXIT_SUCCESS;
+    }
+
+    else
+    {
+        printf("\nOpcion no valida, ejecute nuevamente el programa\n");
+        //getch();
+        return EXIT_SUCCESS;
+    }
 }
--- Tarea02/libreria/cambioFecha.h
+++ Tarea02/libreria/cambioFecha.h
@@ -5,6 +5,73 @@
 #include<vector>
 #include "split.h"
 using namespace std;
+string agregarDia(char Fecha[])
+{
+   int d=1,TotalVector;
+   vector<string> Temporal(0);
+   split(Fecha, *"-", Temporal, TotalVector);
+   char *Dia=strdup(Temporal[2].c_str());
+   char *Mes=strdup(Temporal[1].c_str());
+   char *Anio=strdup(Temporal[0].c_str());
+   int diasMeses[]={31,28,31,30,31,30,31,31,30,31,30,31};
+    int di,m,a;
+    di = atoi(Dia);
+    m = atoi(Mes);
+    a = atoi(Anio);
+    di=di+d;
+    char da[3],me[3],an[3];
+    while(di>diasMeses[m-1])
+       {
+            di=di-diasMeses[m-1];
+            m=m+1;
+            if(m>12)
+            {
+                a=a+1;
+                m=1;
+            }
+       }
+       if(di<10) sprintf(da, "0%d", di);
+       else sprintf(da, "%d", di);
+       if(m<10) sprintf(me, "0%d",m);
+       else sprintf(me, "%d", m);
+       sprintf(an, "%d", a);
+     Temporal.empty();
+   string Fechafinal=(string)an+"-"+(string)me+"-"+(string)da;
+    return Fechafinal;
+   }
+
+int getDia(char Fecha[])
+{
+    vector<string> Temporal(0);
+   int TotalVector;
+   split(Fecha, *"-",  Temporal, TotalVector);
+   char *Dia=strdup(Temporal[2].c_str());
+   int d=atoi(Dia);
+   Temporal.empty();
+    return d;
+}
+
+int getMes(char Fecha[])
+{
+    vector<string> Temporal(0);
+   int TotalVector;
+   split(Fecha, *"-",  Temporal, TotalVector);
+   char *Mes=strdup(Temporal[1].c_str());
+   int m=atoi(Mes);
+   Temporal.empty();
+    return m;
+}
+
+int getAnio(char Fecha[])
+{
+    vector<string> Temporal(0);
+   int TotalVector;
+   split(Fecha, *"-",  Temporal, TotalVector);
+   char *Anio=strdup(Temporal[0].c_str());
+   int a=atoi(Anio);
+    return a;
+}
+
 
 string cambioFecha(char Fecha[])
 {
@@ -27,13 +94,50 @@ string cambioFecha(char Fecha[])
        else if (strcmp(MesAux,"10")==0) strcpy(Mes,"Oct");
        else if (strcmp(MesAux,"11")==0) strcpy(Mes,"Nov");
        else if (strcmp(MesAux,"12")==0) strcpy(Mes,"Dec");
-       char Fechaf[11];
-    strcat(Fechaf,Dia);
-    strcat(Fechaf,"/");
-    strcat(Fechaf,Mes);
-    strcat(Fechaf,"/");
-    strcat(Fechaf,Anio);
-    string Fechafinal=Fechaf;
+       Temporal.empty();
+    string Fechafinal=(string)Dia+"/"+(string)Mes+"/"+(string)Anio;
     return Fechafinal;
 }
+ int compara(char Fechai[],char Fechaf[])
+   {
+   int TotalVector;
+   vector<string> Temporali(0);
+   vector<string> Temporalf(0);
+   split(Fechai, *"-", Temporali, TotalVector);
+   split(Fechaf, *"-", Temporalf, TotalVector);
+   char *Diai=strdup(Temporali[2].c_str());
+   char *Mesi=strdup(Temporali[1].c_str());
+   char *Anioi=strdup(Temporali[0].c_str());
+   char *Diaf=strdup(Temporalf[2].c_str());
+   char *Mesf=strdup(Temporalf[1].c_str());
+   char *Aniof=strdup(Temporalf[0].c_str());
+   int diasMeses[]={31,28,31,30,31,30,31,31,30,31,30,31};
+    int di,mi,ai,df,mf,af;
+    di = atoi(Diai);
+    mi = atoi(Mesi);
+    ai = atoi(Anioi);
+    df = atoi(Diaf);
+    mf = atoi(Mesf);
+    af = atoi(Aniof);
+       if (ai<af)
+           return  -1;
+       else
+          if (ai>af)
+             return 1;
+          else
+            if (mi<mf)
+                return -1;
+            else
+                if (mi>mf)
+                    return 1;
+                else
+                    if (di<df)
+                        return -1;
+                    else
+                        if (di>df)
+                            return 1;
+                        else
+                            return 0;
+   }
+
 #endif // CAMBIOFECHA_H_INCLUDED
