08f9c7e8a5c79d4a2edadd32858fc54c06ce1f63
nelson4722 <nelson4722@gmail.com>
2015-04-28 00:10:03 -0300

    Correccion de movimientos Blancas y Negras

--- Proyecto Damas/tablero.cpp
+++ Proyecto Damas/tablero.cpp
@@ -1,88 +1,121 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <iostream>
-#define T 8
+#define T 10
 
 using namespace std;
 char tablero[T][T];
-char cambio (int i)
+void inicio()
+{
+    cout<<"\t================\n";
+    cout<<"\tTABLERO DE DAMAS\n";
+    cout<<"\t================\n";
+    cout<<endl;
+}
+
+bool direc(char d)
+{
+  if(d=='i')return true;
+  else if(d=='d')return false;
+}
+
+char cambioN (int i)
 {
     char letras[] = {'A','B','C','D','E','F','G','H','I','J'};
-    return letras[i];
+    if(i==1) return letras[0];
+    else if(i==2) return letras[1];
+    else if(i==3) return letras[2];
+    else if(i==4) return letras[3];
+    else if(i==5) return letras[4];
+    else if(i==6) return letras[5];
+    else if(i==7) return letras[6];
+    else if(i==8) return letras[7];
+    else if(i==9) return letras[8];
+    else if(i==10) return letras[9];
 }
-void moverFichaRival(bool izq)
+int cambioL (char i)
 {
-    int i=2;
+    int numeros[] = {1,2,3,4,5,6,7,8,9,10};
+    if(i=='A') return numeros[0];
+    else if(i=='B') return numeros[1];
+    else if(i=='C') return numeros[2];
+    else if(i=='D') return numeros[3];
+    else if(i=='E') return numeros[4];
+    else if(i=='F') return numeros[5];
+    else if(i=='G') return numeros[6];
+    else if(i=='H') return numeros[7];
+    else if(i=='I') return numeros[8];
+    else if(i=='J') return numeros[9];
+}
+void moverFichaRival(int i, int j,bool iz)
+{
     char aux;
     cout<<endl;
-     cout<<endl;
-    aux=tablero[i][i];
-    if(izq==true)
+    cout<<endl;
+    aux=tablero[i][j];
+    if(iz==true)
     {
         //moviemiento por izquierda
-        tablero[i][i]=tablero[i+1][i-1];
-        tablero[i+1][i-1]=aux;
-
+        tablero[i][j]=tablero[i+1][j-1];
+        tablero[i+1][j-1]=aux;
     }
-    else
+    else if(iz==false)
     {
         //moviemiento por derecha
-        tablero[i][i]=tablero[i+1][i+1];
+        tablero[i][j]=tablero[i+1][i+1];
         tablero[i+1][i+1]=aux;
     }
-
-
 }
-void moverFicha(bool izq)
+void moverFicha(int i, int j,bool iz)
 {
-    int i=5;
     char aux;
     cout<<endl;
-     cout<<endl;
-    aux=tablero[i][i];
-    if(izq==true)
+    cout<<endl;
+    aux=tablero[i][j];
+    if(iz==true)
     {
         //moviemiento por izquierda
-        tablero[i][i]=tablero[i-1][i-1];
-        tablero[i-1][i-1]=aux;
+        tablero[i][j]=tablero[i-1][j-1];
+        tablero[i-1][j-1]=aux;
     }
-    else
+    else if(iz==false)
     {
         //moviemiento por derecha
-        tablero[i][i]=tablero[i-1][i+1];
-        tablero[i-1][i+1]=aux;
+        tablero[i][j]=tablero[i-1][j+1];
+        tablero[i-1][j+1]=aux;
     }
 
 }
 
 void llenarTablero(){
+
 	for(int i = 0; i < T; i++)
 	        for(int j = 0; j < T; j++)
 		{
 			if (i < 3 && ((i + j) % 2 == 0))
-				tablero[i][j] = 'O';
+				tablero[i][j] = 'N';
 			else
-                if(i == 3 && ((i + j) % 2 == 0))
-                   tablero[i][j] = '+';
-                   else
-                    if(i == 4 && ((i + j) % 2 == 0))
+                if((i==3||i==4||i==5||i==6) && ((i + j) % 2 == 0))
                    tablero[i][j] = '+';
             else
-				if ((i > 4) && ((i + j) % 2 == 0))
-					tablero[i][j] = 'P';
+				if ((i > 6) && ((i + j) % 2 == 0))
+					tablero[i][j] = 'B';
 				else
 					tablero[i][j] = '-' ;
 		}
 }
 
 void imprimirTablero(){
+    system("/usr/bin/clear");
+
 	for(int i = 0; i < T; i++)
     {
 		if (i == 0)
 		{
 		    cout<<"  ";
 			for (int k = 0; k < T; k++)
-				cout<<" "<<cambio(k)<<"  ";
+				cout<<" "<<cambioN(k+1)<<"  ";
 			cout<<endl;
 			cout<<endl;
 		}
@@ -95,20 +128,25 @@ void imprimirTablero(){
           	cout<<endl;
           	cout<<endl;
     	}
+    	inicio();
 }
 
 int main(){
-        bool izq=false;
-        cout<<"\t================\n";
-        cout<<"\tTABLERO DE DAMAS\n";
-        cout<<"\t================\n";
-        cout<<endl;
-
+        int i,j;
+        char jota,direccion;
+        bool dir;
     	llenarTablero();
     	imprimirTablero();
-    	moverFichaRival(izq);
+        cout<<"ingrese posicion que desea mover y direccion de Blancos"<<endl;
+    	cin>>jota>>i>>direccion;
+    	dir=direc(direccion);
+    	moverFicha(i-1,cambioL(jota)-1,dir);
     	imprimirTablero();
-    	moverFicha(!izq);
+        cout<<"ingrese posicion que desea mover y direccion de Negros"<<endl;
+    	cin>>jota>>i>>direccion;
+    	dir=direc(direccion);
+    	moverFichaRival(i-1,cambioL(jota)-1,dir);
     	imprimirTablero();
+
 	return 0;
 }
